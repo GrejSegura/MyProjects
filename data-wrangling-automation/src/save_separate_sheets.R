851470628be8de9f35d71b39cdb73c67f63720b4
Grejell Segura <mr_grej@yahoo.com>
2019-04-24 16:46:25 +0400

    Update save_separate_sheets.R

--- data-wrangling-automation/src/save_separate_sheets.R
+++ data-wrangling-automation/src/save_separate_sheets.R
@@ -4,7 +4,7 @@ options(java.parameters = "-Xmx4g")  ## memory control in RJava
 library(xlsx)
 library(XLConnect)
 
-final2 <- read.csv("C:/Users/Grejell/Documents/Data Analysis/Automation Project/gessSessions/input/gessformatted_v2.csv")
+final2 <- read.csv("./input/formatted_v2.csv")
 
 sessions <- unique(final2$Session)
 sessions <- sessions[order(sessions)]
@@ -23,9 +23,9 @@ for (i in sessions){
 	
 	data <- final2[final2$Session == i, ]
 	
-	write.xlsx(data, paste("C:/Users/Grejell/Documents/Data Analysis/Automation Project/gessSessions/output/data.xlsx"), sheetName = i, append = TRUE, row.names = FALSE)
+	write.xlsx(data, paste("./output/data.xlsx"), sheetName = i, append = TRUE, row.names = FALSE)
 	xlcFreeMemory()  ## memory control in excel - this prevents the error "Error in .jcall("RJavaTools", "Ljava/lang/Object;", "invokeMethod", cl,  : java.lang.OutOfMemoryError: GC overhead limit exceeded
 }
 
 library(readxl)
-length(excel_sheets("C:/Users/Grejell/Documents/Data Analysis/Automation Project/gessSessions/output/data.xlsx")) ## to check if all are saved accordingly - match the number to the actual number of sessions/shows
+length(excel_sheets("./output/data.xlsx")) ## to check if all are saved accordingly - match the number to the actual number of sessions/shows
